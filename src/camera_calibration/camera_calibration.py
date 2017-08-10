0e2b6aa597c637f1fdd5469e1b83120425da9eb3
lingjiesang <sanglingjie@gmail.com>
2017-08-10 00:04:33 -0400

    refactored camera calibration

--- src/camera_calibration/camera_calibration.py
+++ src/camera_calibration/camera_calibration.py
@@ -1,8 +1,62 @@
 import pickle
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import os
+import glob
+
+
+def find_corners(image_paths, image_output_folder, row_number, col_number,
+                 obj_points_filename=None, img_points_filename=None):
+    """
+    find chess board corners of images whose paths are given, save images with corner drawn in the given output folder
+    also save the object points and image points in npy files
+    :param image_paths: paths of images containing chess board
+    :param image_output_folder: folder to save images with corner drawn
+    :param row_number: number of rows of inner corners on the chess board
+    :param col_number: number of columns of inner corners on the chess board
+    :param obj_points_filename: filename to save the object points
+    :param img_points_filename: filename to save the image points
+    :return: object points and image points
+    """
+
+    if not os.path.exists(image_output_folder):
+        os.makedirs(image_output_folder)
+
+    # Arrays to store object points and image points from ALL the images.
+    obj_points = []  # 3d points in real world space
+    img_points = []  # 2d points in image plane.
+
+    # prepare object points for a SINGLE image, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    obj_point = np.zeros((row_number * col_number, 3), np.float32)
+    obj_point[:, :2] = np.mgrid[0:col_number, 0:row_number].T.reshape(-1, 2)
+
+    # Step through the list of calibration images and search for chessboard corners
+    for idx, file_name in enumerate(image_paths):
+
+        if 'jpg' in file_name:
+            img = cv2.imread(file_name)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # Find the chessboard corners
+            ret, corners = cv2.findChessboardCorners(gray, (col_number, row_number), None)
+
+            # If found, add object points, image points
+            if ret:
+                obj_points.append(obj_point)
+                img_points.append(corners)
+
+                # Draw and display the corners
+                cv2.drawChessboardCorners(img, (col_number, row_number), corners, ret)
+                write_name = os.path.join(image_output_folder, file_name.split('/')[-1])
+                cv2.imwrite(write_name, img)
+                # cv2.imshow('img', img)
+                # cv2.waitKey(500)
+
+    # cv2.destroyAllWindows()
+    if obj_points_filename is not None and img_points_filename is not None:
+        np.save(obj_points_filename, obj_points)
+        np.save(img_points_filename, img_points)
+    return obj_points, img_points
 
 
 def camera_calibration(obj_points, img_points, img_size, pickle_filename):
@@ -25,43 +79,16 @@ def camera_calibration(obj_points, img_points, img_size, pickle_filename):
     return [mtx, dist]
 
 
-def undistort_image(mtx, dist, image_path, output_path, contrast_path):
-    """
-    undistort an given image, save the undistorted image, and save a contrast plot containing the two images
-    :param mtx: camera matrix from calibration
-    :param dist: distortion coefficient from calibration
-    :param image_path: path of the image to be undistorted
-    :param output_path: path to save the undistorted image
-    :param contrast_path: path to save the contrast plot
-    :return: None
-    """
-    img = cv2.imread(image_path)
-
-    dst = cv2.undistort(img, mtx, dist, None, mtx)
-
-    cv2.imwrite(output_path, dst)
-
-    # dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
-    # Visualize undistortion
-    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
-    ax1.imshow(img)
-    ax1.set_title('Original Image', fontsize=30)
-    ax2.imshow(dst)
-    ax2.set_title('Undistorted Image', fontsize=30)
-    plt.savefig(contrast_path)
-
-
 def main():
-    obj_points_filename = "obj_points.npy"
-    img_points_filename = "img_points.npy"
-    try:
-        obj_points = np.load(obj_points_filename)
-        img_points = np.load(img_points_filename)
-    except FileNotFoundError:
-        print("use find_corners in extract_image_points to save object and image points first")
-        return
-
+    grid_rows = 6
+    grid_columns = 9
     base_dir = os.path.dirname(__file__)
+
+    image_folder = os.path.abspath(os.path.join(base_dir, '..', '..', 'camera_cal/calibration*.jpg'))
+    image_paths = glob.glob(image_folder)
+    image_output_folder = os.path.abspath(os.path.join(base_dir, '..', '..', 'camera_cal_corners'))
+
+    obj_points, img_points = find_corners(image_paths, image_output_folder, grid_rows, grid_columns)
 
     image_name = 'calibration8'
     image_path = os.path.abspath(os.path.join(base_dir, '..', '..', 'camera_cal', image_name + '.jpg'))
@@ -72,10 +99,6 @@ def main():
 
     [mtx, dist] = camera_calibration(obj_points, img_points, img_size,  pickle_filename)
 
-    output_path = os.path.abspath(os.path.join(base_dir, '..', '..', 'camera_cal_corners', image_name + '_undistorted.jpg'))
-    contrast_path = os.path.abspath(os.path.join(base_dir, '..', '..', 'camera_cal_corners', image_name + '_contrast.jpg'))
-
-    undistort_image(mtx, dist, image_path, output_path, contrast_path)
 
 if __name__ == '__main__':
     main()
---
+++ src/camera_calibration/undistort_image.py
@@ -0,0 +1,78 @@
+import pickle
+import cv2
+import matplotlib.pyplot as plt
+import os
+
+
+class Undistort:
+    """
+    Provide functionality to undistort images from a camera whose matrix and distort coefficient are available
+    """
+    def __init__(self):
+        """
+        initiation, read camera matrix and distort coefficient from disk
+        """
+        camera_pickle_path = 'wide_dist_pickle.p'
+
+        try:
+            dist_pickle = pickle.load(open(camera_pickle_path, "rb"))
+            self.mtx = dist_pickle["mtx"]
+            self.dist = dist_pickle["dist"]
+        except Exception:
+            self.mtx = None
+            self.dist = None
+
+    def undistort_image(self, img):
+        """
+        undistort a image
+        raise ValueError if camera calibration are not available
+        :param img: image to be undistroted
+        :return: undistroted image
+        """
+        if self.mtx is not None and self.dist is not None:
+            return cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
+        else:
+            raise ValueError("unable to process the image, camera data not available")
+
+    def plot_contrast(self, original, undistorted, save_path=None):
+        """
+        plot the original and undistorted images together, show the image if save_path is not provided
+        save the image if save path is provided
+        :param original: original image
+        :param undistorted: undistorted image
+        :param save_path:  path to save the image
+        :return: None
+        """
+        original = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
+        undistorted = cv2.cvtColor(undistorted, cv2.COLOR_BGR2RGB)
+        f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+        ax1.imshow(original)
+        ax1.set_title('Original Image', fontsize=30)
+        ax2.imshow(undistorted)
+        ax2.set_title('Undistorted Image', fontsize=30)
+        if save_path is not None:
+            plt.savefig(save_path)
+        else:
+            plt.show()
+
+def main():
+    undistort = Undistort()
+
+    base_dir = os.path.dirname(__file__)
+    image_name = 'calibration1'
+    image_path = os.path.abspath(os.path.join(base_dir, '..', '..', 'camera_cal', image_name + '.jpg'))
+    img = cv2.imread(image_path)
+
+    undistorted_img_save_path = os.path.abspath(
+        os.path.join(base_dir, '..', '..', 'camera_cal_corners', image_name + '_undistorted.jpg'))
+    contrast_images_save_path = os.path.abspath(
+        os.path.join(base_dir, '..', '..', 'camera_cal_corners', image_name + '_contrast.jpg'))
+
+    undistorted_img = undistort.undistort_image(img)
+    cv2.imwrite(undistorted_img_save_path, undistorted_img)
+
+    undistort.plot_contrast(img, undistorted_img, contrast_images_save_path)
+
+
+if __name__ == '__main__':
+    main()
